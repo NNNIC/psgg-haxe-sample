7d2f97e5dc64722c3ef2669ff5718724a6e7501d
nn4343 <gea01441@nifty.com>
2020-08-14 10:47:24 +0900

    no message

--- src/Test.hx
+++ src/Test.hx
@@ -8,23 +8,3 @@ class Test {
         trace("#END");
     }
 }
-/*
-class Sub {
-    var m_curfunc : STATEFUNC;
-    
-    var m_callstacks : Array<STATEFUNC> = [for(i in 0...10) null];
-
-    public function new(){}
-
-    function S_START(bFirst: Bool)  {
-        trace("S_START");
-    }
-    public function Set() {
-        m_callstacks[0] = S_START;
-    }
-    public function Run() {
-        var func : Bool->Void = m_callstacks[0];
-        func(true);
-    }
-}
-*/
